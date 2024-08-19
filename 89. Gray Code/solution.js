031a30232e240db49d9c55d372f49f1f1fa3eb15
Fabiana Liria Soto <117101392+fabiliria280802@users.noreply.github.com>
2024-08-19 15:57:07 -0500

    chore: Add gray code generation algorithm

---
+++ 89. Gray Code/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+
+var grayCode = function(n) {
+    let result = [];
+    for (let i = 0; i < (1 << n); i++) {
+        result.push(i ^ (i >> 1));
+    }
+    return result;
+};
+
+let n = 2;
+console.log(grayCode);
